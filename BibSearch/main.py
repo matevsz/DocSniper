6280f5f15803eebcbebd3a978974c1a968ea3d16
Mariusz Kozakowski <11mariom+wordpress@gmail.com>
2016-06-01 20:47:25 +0200

    Command Line Interface

--- BibSearch/main.py
+++ BibSearch/main.py
@@ -5,9 +5,23 @@
 
 """Main program"""
 
+import argparse
 from ScienceDirect import ScienceDirect
 
-sd = ScienceDirect("artificial intelligence")
-sd.search()
-sd.get_results()
-sd.get_bibtex()
+FLAGS = argparse.ArgumentParser()
+FLAGS.add_argument("-q", "--query", required=True, help="search string")
+FLAGS.add_argument("-b", "--bibtex", action="store_true", help="return bibtex url")
+FLAGS = FLAGS.parse_args()
+ARGS = FLAGS
+
+if ARGS.query and len(ARGS.query) > 3:
+    SEARCH_ENGINE = ScienceDirect(ARGS.query)
+else:
+    print "Wrong query! Search query should have at least 3 characters"
+    exit(1)
+
+SEARCH_ENGINE.search()
+SEARCH_ENGINE.get_bibtex_url()
+
+if ARGS.bibtex:
+    SEARCH_ENGINE.print_bibtex_url()
